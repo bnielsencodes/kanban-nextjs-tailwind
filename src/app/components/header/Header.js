8ca07b19e1b61f85a1a8a2d2d960e2c99a8ca87f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-08-27 22:51:22 -0700

    Add styles

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -4,6 +4,9 @@ import HeaderRight from "./HeaderRight";
 export default function Header(props) {
   return (
     <div
+      className={`flex items-center justify-between p-4 ${
+        props.darkMode ? "bg-neutral-300" : "bg-neutral-800"
+      }`}
     >
       <HeaderLeft
         darkMode={props.darkMode}
