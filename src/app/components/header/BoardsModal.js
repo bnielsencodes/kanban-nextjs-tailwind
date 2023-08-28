4f7a4bc79a529b952922aaf1f8dc36e6e2c9f762
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-08-27 23:06:15 -0700

    Add styles

--- src/app/components/header/BoardsModal.js
+++ src/app/components/header/BoardsModal.js
@@ -4,9 +4,21 @@ import LightDarkToggle from "@/app/components/shared/LightDarkToggle";
 export default function BoardsModal(props) {
   return (
     <div
+      className={`absolute top-[43px] left-[-56px] z-30 w-screen h-screen select-none cursor-default ${
+        props.showBoardsModal ? "opacity-100" : "opacity-0"
+      } ${props.showBoardsModal ? "visible" : "invisible"}`}
     >
       <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
       <div
+        className={`boards-modal relative z-40 flex flex-col w-[264px] py-4 pt-[17px] rounded-lg mt-4 mx-auto mb-0 ${
+          props.darkMode ? "bg-neutral-300" : "bg-neutral-800"
+        } ${
+          props.darkMode ? "shadow-boardsModalDark" : "shadow-boardsModalLight"
+        } select-none ${
+          props.showBoardsModal
+            ? "opacity-100 visible translate-y-0"
+            : "opacity-0 invisible -translate-y-2/4"
+        }`}
       >
         <AllBoards
           setShowBoardsModal={props.setShowBoardsModal}
