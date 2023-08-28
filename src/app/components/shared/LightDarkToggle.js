728a4522df13930d9a41eb327f7e8328b01c0d0c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-08-27 22:49:48 -0700

    Add styles

--- src/app/components/shared/LightDarkToggle.js
+++ src/app/components/shared/LightDarkToggle.js
@@ -5,6 +5,9 @@ import iconDark from "/public/assets/icon-dark-theme.svg";
 export default function LightDarkToggle(props) {
   return (
     <div
+      className={`flex items-center justify-center w-[235px] h-12 rounded-md mt-auto mx-auto ${
+        props.darkMode ? "bg-neutral-200" : "bg-neutral-700"
+      }`}
     >
       <Image
         src={iconLight}
@@ -16,15 +19,18 @@ export default function LightDarkToggle(props) {
       <div className="relative inline-block w-10 mx-6 ">
         <input
           id="toggle"
+          className="toggle hidden"
           type="checkbox"
           name="toggle"
         />
         <label
+          className="block rounded-[20px] overflow-hidden cursor-pointer"
           htmlFor="toggle"
         >
           <span
+            className={`inner block w-[200px] ${props.darkMode ? "dark" : ""}`}
           />
-          <span />
+          <span className={`switch ${props.darkMode ? "dark" : ""}`} />
         </label>
       </div>
       <Image
