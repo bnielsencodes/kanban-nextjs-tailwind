51fe7956c49287cc09afe7bed625f9b88f8cb35f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-08-27 22:52:31 -0700

    Add styles

--- src/app/components/header/BoardSelect.js
+++ src/app/components/header/BoardSelect.js
@@ -6,9 +6,18 @@ export default function BoardSelect(props) {
   return (
     <div className="relative flex items-center cursor-pointer group">
       <input
+        id="board-select-btn"
+        className="appearance-none hidden focus-visible:outline-none"
+        type="checkbox"
+        name="board-select"
       />
       <label
+        className={`flex items-center gap-4 pr-[19px] mt-1 ${
+          props.darkMode ? "text-neutral-800" : "text-neutral-100"
+        } text-lg font-bold leading-[23px] select-none group-hover:cursor-pointer`}
+        htmlFor="board-select-btn"
       >
+      </label>
         <Image
           className="absolute top-[7px] right-0 mt-[3px] pointer-events-none"
           src={iconChevronDown}
