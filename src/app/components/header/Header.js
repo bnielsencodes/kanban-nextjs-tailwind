c32c536a0bc500311f6358af8cb1bdee0e056665
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-09-04 22:47:29 -0700

    Create state

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import HeaderLeft from "./HeaderLeft";
 import HeaderRight from "./HeaderRight";
 
 export default function Header(props) {
+  const [showEditDeleteBoardModal, setShowEditDeleteBoardModal] =
+    useState(false);
+
   return (
     <div
       className={`flex items-center justify-between p-4 ${
